c6f2998aed833f28ea9dbedd771b54c66a113ed6
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-06-30 10:56:56 -0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { headers } from "next/headers";
-
 import { cookieToInitialState } from 'wagmi'
 import { config } from "@/utils/config";
 import Web3ModalProvider from "@/utils/context";
 import Navbar from "@/components/Navbar";
+import CpxResearch from "cpx-research-sdk-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,10 +25,26 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Web3ModalProvider initialState={initialState}>
+          <CpxResearch
+            accentColor="#ff9800"
+            appId={process.env.NEXT_PUBLIC_CPX_APP_ID || ""}
+            userId="2" // You might want to replace this with a dynamic user ID
+            notificationWidget={{
+              backgroundColor: "#ff9800",
+              height: 60,
+              isSingleSurvey: true,
+              position: "bottom",
+              roundedCorners: 10,
+              text: "Click me for surveys",
+              textColor: "#ffffff",
+              textSize: 18,
+              width: 320,
+            }}
+          />
           <Navbar />
           {children}
-          </Web3ModalProvider>
-        </body>
+        </Web3ModalProvider>
+      </body>
     </html>
   );
 }
