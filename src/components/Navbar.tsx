f64382cb399c581677a0b1552a4c7f68b23cf6f2
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-12 18:02:26 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='flex w-full text-white py-4 px-8 bg-[#f05e23]'>
+        <div className='flex w-full text-black py-4 px-8 bg-[#f05e23]'>
             <div className='flex flex-row justify-between w-full'>
                 <div>
-                    REAP MiniTap
+                    REAP 
                 </div>
                 <div>
                     
