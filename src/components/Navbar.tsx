692dffd545cfb2481c26f1f4fad64b297cda5748
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-12 11:05:51 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='flex w-full text-white py-4 px-8 bg-slate-800'>
+        <div className='flex w-full text-white py-4 px-8 bg-orange-500'>
             <div className='flex flex-row justify-between w-full'>
                 <div>
-                    LOGO
+                    Mini Tap
                 </div>
                 <div>
-                    Mid Part
+                    
                 </div>
                 {/* <div className='hidden md:flex'>
                 <w3m-button />
