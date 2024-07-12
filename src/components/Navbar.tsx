b6f415c9faff4832f79a69535332bb174602244f
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-12 18:06:15 -0400

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <div className='flex w-full text-black py-4 px-8 bg-[#f05e23]'>
+        <div className='flex w-full text-[#f05e23] py-4 px-8 bg-black'>
             <div className='flex flex-row justify-between w-full'>
                 <div>
                     REAP 
