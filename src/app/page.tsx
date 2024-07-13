ca57bb9345f1aa5121aa5f87ffa100a9f1a44451
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-07-13 11:36:38 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Celon from "@/components/Celon";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="flex-grow" /> {/* This pushes the content down */}
       <Celon />
+      <div className="flex-grow flex-grow-[2]" /> {/* This pushes the content up more */}
     </main>
   );
 }
